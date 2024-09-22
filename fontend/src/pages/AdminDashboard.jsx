f0815c15e4c690c04a969e8546c22bde9ea762b0
DvHuy <ahuyahihi4@gmail.com>
2024-09-23 06:27:35 +0700

    Dashboard Page Fontend

--- fontend/src/pages/AdminDashboard.jsx
+++ fontend/src/pages/AdminDashboard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useAuth } from "../context/authContext";
 import AdminSidebar from "../components/dashboard/AdminSidebar";
+import Navbar from "../components/dashboard/Navbar";
+import AdminSumary from "../components/dashboard/AdminSumary";
 
 const AdminDashboard = () => {
   const { user } = useAuth();
 
   return (
-    <div>
-      <AdminSidebar/>
+    <div className="flex">
+      <AdminSidebar />
+      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+        <Navbar />
+        <AdminSumary/>
+      </div>
     </div>
-  )
+  );
 };
 
 export default AdminDashboard;
